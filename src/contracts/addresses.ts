a3bbbcbcdaf4e05b3d843df5ab111e8aa16e9bde
0xlenny <46480795+0xlny@users.noreply.github.com>
2024-07-08 09:47:12 +0200

    :arrow_up: Update UniV2 deployments (#7)

--- src/contracts/addresses.ts
+++ src/contracts/addresses.ts
@@ -1,4 +1,4 @@
 // Addresses on the kakarot testnet (TODO add testnet/mainnet environments)
-export const KAKASWAP_ROUTER = "0x5D9dE86E1783ae719D6c47c35Cbe08d937F3BeCd";
-export const KAKASWAP_FACTORY = "0xD162aF6380D83D55c584E7a538ec7Bb6e773599D";
+export const KAKASWAP_ROUTER = "0x7c51B6E67a3a7deD9E61F555e1f9FFb9BdE7ae4d";
+export const KAKASWAP_FACTORY = "0xe4CEb28E1e3a6192542A778f1c7277b7267C00B8";
 export const WETH_ADDRESS = "0x761612F0C8bdf8cF10e6F10045E2Ca7cbffBa8A3";
